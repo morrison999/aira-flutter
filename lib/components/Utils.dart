d4b31fee10d9a3cec5f9691ced991536337db26e
Dave Morrison <djm99999@gmail.com>
2021-03-26 07:28:01 -0500

    add button grid

--- lib/components/Utils.dart
+++ lib/components/Utils.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 
-void airaAnnounce(String text) {
-  SemanticsService.announce(text, TextDirection.ltr,);
+Future<void> airaAnnounce(String text) async {
+  await SemanticsService.announce(text, TextDirection.ltr,);
 }
 void airaErrorSnackbar(String title, [String message= ""]) {
     Get.snackbar(title, message, snackPosition: SnackPosition.BOTTOM, backgroundColor: ThemeData().errorColor);
--- lib/view/authenticate.dart
+++ lib/view/authenticate.dart
@@ -4,18 +4,37 @@ import 'package:accessibility/view/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'login_page.dart';
+import '../components/Utils.dart';
 
 class AuthenticationPage extends StatelessWidget {
-
+  final rows = 5;
+  final columns = 4;
+  final rowHeight = 40.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AiraAppBar(titleIn: "Welcome Page"),
         body: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Container(
+                width: 400.0,
+                height: rows*rowHeight,
+                child: GridView.count(
+                  childAspectRatio: 2,
+                  crossAxisCount: columns,
+                  children: List<Widget>.generate(rows*columns, (index) {
+                    return Container(
+                        height: rowHeight,
+                        child: ElevatedButton(
+                          onPressed: () => buttonPressed(index),
+                          child: Text("${index+1}")),
+                      );
+                  }),
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 30.0),
                 child: Text(
@@ -29,15 +48,18 @@ class AuthenticationPage extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: ElevatedButton(
-                      child: Text("Login"),
+                      child: Text("Login",),
                       onPressed: _dummy,
                     ),
                   ),
-                  Text("or", style: TextStyle(fontSize: AiraTextSize.getSmall())),
+                  Text("or",
+                      style: TextStyle(fontSize: AiraTextSize.getSmall())),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: ElevatedButton(child: Text("Register"),
-                      onPressed: _toRegisterPage,),
+                    child: ElevatedButton(
+                      child: Text("Register"),
+                      onPressed: _toRegisterPage,
+                    ),
                   ),
                 ],
               ),
@@ -45,10 +67,19 @@ class AuthenticationPage extends StatelessWidget {
           ),
         ));
   }
+
   void _toRegisterPage() {
     Get.to(RegisterPage());
   }
+
   void _dummy() {
-    Get.defaultDialog(title: "Unsupported",middleText: "Function not implemented");
+    Get.defaultDialog(
+        title: "Unsupported", middleText: "Function not implemented");
+  }
+
+  buttonPressed(int index) {
+    airaAnnounce("You pressed button ${index+1}");
+    Get.snackbar("You pressed Button ${index+1}", "",
+        snackPosition: SnackPosition.BOTTOM);
   }
 }
